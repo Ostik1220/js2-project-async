677f46ead24deb88f3c7bb76282937bb1c43f44c
Ostik <117195748+Ostik1220@users.noreply.github.com>
2025-07-27 01:23:21 +0300

    functional done

--- src/operations/getPostsApi.js
+++ src/operations/getPostsApi.js
@@ -14,22 +14,27 @@ export const getPosts = async() => {
 
 function renderPosts(posts) {
   return posts.map((object) => {
+    const commentsHTML = (object.comments || [])
+      .map((comment) => `<li>${comment}</li>`)
+      .join("");
+
     return `
 <div class="post" id="${object.id}">
-<h2>${object.title}</h2>
-<p>${object.text}</p>
-<button class="editPostButton" data-id="">Редагувати</button>
-<button class="deletePostButton" data-id="">Видалити</button>
-<div class="commentsContainer" data-id="">
-<h3>Коментарі:</h3>
-<ul>
-<li></li>
-</ul>
-<form class="createCommentForm">
-<input type="text" class="commentInput" placeholder="Новий коментар" required>
-<button type="submit">Додати коментар</button>
-</form>
-</div>
+  <h2>${object.title}</h2>
+  <p>${object.text}</p>
+  <button class="editPostButton" data-id="${object.id}">Редагувати</button>
+  <button class="deletePostButton" data-id="${object.id}">Видалити</button>
+  
+  <div class="commentsContainer" data-id="${object.id}">
+    <h3>Коментарі:</h3>
+    <ul class="commentsList">
+      ${commentsHTML}
+    </ul>
+    <form class="createCommentForm">
+      <input type="text" class="commentInput" placeholder="Новий коментар" required>
+      <button type="submit">Додати коментар</button>
+    </form>
+  </div>
 </div>
     `;
   });
